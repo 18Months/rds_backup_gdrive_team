4c6a7c41d13bfe8756a6c9601906f44c33f11ebb
Samuele Bistoletti <s.bistoletti@18months.it>
2018-11-09 18:52:44 +0100

    Added option to delete old backups

--- README.md
+++ README.md
@@ -39,6 +39,7 @@ These are some of the environment variables you will need to pass to the Docker 
 The container needs to be run in the background to capture and replace rclone environment variables into the `rclone.conf` file.
 Cron rule backup runs everyday at hour and minute defined in CRON_H and CRON_M variables.
 Set timezone in TZ format as listed here [Timezones](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones).
+Set CLEAN_D_START and CLEAN_D_END for backup cleanup, process will delete backup files older than CLEAN_D_START, but newer than CLEAN_D_END.
 
 ```
 $ docker run -d --restart unless-stopped \
@@ -55,5 +56,7 @@ $ docker run -d --restart unless-stopped \
   -e "TIMEZONE=UTC" \
   -e "CRON_H=0" \
   -e "CRON_M=0" \
+  -e "CLEAN_D_START=5" \
+  -e "CLEAN_D_END=10" \
   eighteenmonths/rds_backup_gdrive_team:latest
 ```
--- scripts/pg_dump.sh
+++ scripts/pg_dump.sh
@@ -2,17 +2,23 @@
 
 echo "Backup started."
 
-filename=${PG_DATABASE}_$(date +"%Y%m%d").sql.gz
-obsolete_filename=${PG_DATABASE}_$(date --date="5 days ago" +"%Y%m%d").sql.gz
+filename=${PG_DATABASE}_$(date +"%Y%m%d%H%M").sql.gz
 
 pg_dump -h $PG_HOST -p $PG_PORT -U $PG_USER -c $PG_DATABASE | gzip > /root/$filename
 
 echo "Backup finished."
 
-echo "Copying backup to GDrive."
+echo "Copying backup to GDrive..."
+rclone copy /root/$filename remote:/
+echo "Backup copied to GDrive."
 
-rclone copy /root/$filename remote:/ -vv
-rclone delete remote:/$obsolete_filename -vv
+echo "Deleting tmp backup copy..."
 rm /root/$filename
 
-echo "Backup copied to GDrive."
+echo "Deleting old backups..."
+
+for x in $(seq $CLEAN_D_START $CLEAN_D_END); do 
+  rclone lsf remote:/ | grep ${PG_DATABASE}_$(date --date="${x} days ago" +"%Y%m%d") | xargs -I{} rclone delete remote:/{}
+done
+
+echo "Process complete."
